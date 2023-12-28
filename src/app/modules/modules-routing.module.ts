e280034ce0f4dce469b10044c370d583a4a06c70
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-29 00:59:22 +0100

    add a top level routing for modules

---
+++ src/app/modules/modules-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const moduleRoutes: Routes = [];
+
+@NgModule({
+    imports: [RouterModule.forChild(moduleRoutes)],
+    providers: [RouterModule]
+})
+
+export class ModulesRoutingModule {}
