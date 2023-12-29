4bb91509c91501d167e45973c5b5f1c7e5ffb37d
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-29 11:38:32 +0100

    add offer card component + add offer card preview component + add boxicons lib

---
+++ src/app/modules/home/components/job-preview-card/job-preview-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-preview-card',
+  templateUrl: './job-preview-card.component.html',
+  styleUrls: ['./job-preview-card.component.less']
+})
+export class JobPreviewCardComponent {
+  description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tenetur laboriosam facere enim adipisci esse dolorum praesentium quas ducimus beatae dolorem velit, doloremque voluptas nemo corporis unde architecto delectus optio maxime suscipit"
+}
