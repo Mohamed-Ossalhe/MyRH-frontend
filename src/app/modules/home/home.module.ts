4997e4efe393b8371cabeb52b3c5a1fa691b10d5
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-29 19:36:04 +0100

    add offers search bar + add reusable button and input components

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from '@modules/home/pages/home/home.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { JobOffersComponent } from './pages/job-offers/job-offers.component';
-import { JobOfferCardComponent, JobPreviewCardComponent } from './components';
+import { JobOfferCardComponent, JobPreviewCardComponent, JobOffersSearchComponent } from './components';
 
 
 
@@ -13,7 +13,8 @@ import { JobOfferCardComponent, JobPreviewCardComponent } from './components';
     HomeComponent,
     JobOffersComponent,
     JobOfferCardComponent,
-    JobPreviewCardComponent
+    JobPreviewCardComponent,
+    JobOffersSearchComponent
   ],
   imports: [
     CommonModule,
